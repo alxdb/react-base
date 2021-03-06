673eaa51e367fd60ef9022aa05b9f163ef10d30c
Alexander Davidson Bryan <alxdb@pm.me>
2021-03-06 13:33:34 +0000

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import _ from "lodash";
+
+import "./main.css";
+
+const App = () => <h1> Hello World! </h1>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.ts
@@ -0,0 +1,36 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config: webpack.Configuration = {
+  mode: "development",
+  entry: "./src/main.tsx",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ],
+};
+
+export default config;
